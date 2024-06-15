f121d59648374139371c3830acb30a0c62b5ee7b
anastasia <Anastasiya.Gribovskaya@forge.by>
2024-06-15 17:54:42 +0300

    feat: completed the task Hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
+import { createHash } from 'node:crypto';
+import fs from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const currentDir = dirname(fileURLToPath(import.meta.url));
+    const pathToFile = path.join(currentDir, 'files', 'fileToCalculateHashFor.txt');
+
+    const hash = createHash('sha256');
+    hash.on('readable', () => {
+        const data = hash.read();
+        if (data) {
+            console.log(data.toString('hex'));
+        }
+    })
+
+    fs.readFile(pathToFile, 'utf-8', (err, data) => {
+        if (err) {
+            hash.end();
+            throw err;
+        }
+        hash.write(data);
+        hash.end();
+    })
 };
 
 await calculateHash();
