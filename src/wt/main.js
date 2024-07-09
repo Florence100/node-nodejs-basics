56737907285ef78bdca7e079565ac735a32ef12f
anastasia <Anastasiya.Gribovskaya@forge.by>
2024-07-09 12:12:04 +0300

    feat: complete the task Worker Threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,43 @@
+import { Worker } from 'worker_threads';
+import path, { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { cpus } from 'os';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname  = dirname(__filename);
+
+const pathToWorker = path.join(__dirname, 'worker.js');
+const cpuCore = cpus().length;
+
 const performCalculations = async () => {
-    // Write your code here
+    const promises = [];
+    let startNum = 10;
+
+    for (let i = 0; i < cpuCore; i ++) {
+        promises.push(
+            new Promise(function(resolve, reject) {
+                const worker = new Worker(pathToWorker, {workerData: {num: startNum}});
+                worker.on('message', (result) => {
+                    const data = {
+                        status: 'resolved',
+                        data  : result
+                    }
+                    resolve(data);
+                })
+                worker.on('error', (err) => {
+                    const data = {
+                        status: 'error',
+                        data  : null
+                    }
+                    reject(data);
+                })
+            })
+        )
+        startNum += 1;
+    }
+
+    const result = (await Promise.allSettled(promises)).map((x => x.value || x.reason));
+    console.log(result);
 };
 
 await performCalculations();
